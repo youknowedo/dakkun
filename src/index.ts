ae4d16728b6e6b9a92afa8cc2eee6a69c0e50ce0
Filip Mårtensson <fenwikk@protonmail.com>
2024-08-16 00:05:01 +0200

    dakkun/down

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-import { Hono } from 'hono'
+import { WebClient } from "@slack/web-api";
+import { Hono } from "hono";
 
-const app = new Hono()
+const web = new WebClient();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono();
 
-export default app
+app.get("/dakkun/down", async (c) => {
+    const isDown = await fetch("https://hackhour.hackclub.com/status")
+        .then((res) => {
+            if (res.ok) return res.json();
+        })
+        .then((data) => {
+            return !data.slackConnected;
+        })
+        .catch(() => {
+            return true;
+        });
+
+    return c.text(
+        JSON.stringify({ text: isDown ? "down hakkun is" : "up hakkun is" })
+    );
+});
+
+export default app;
